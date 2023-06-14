03b1ba35ed22e984a561aa399f99f4f226c34c5a
cake monotone <cake.monotone@gmail.com>
2023-06-14 13:12:47 +0900

    append select code into exmple/memory_storage_usage

--- pkg/rust/examples/memory_storage_usage.rs
+++ pkg/rust/examples/memory_storage_usage.rs
@@ -1,6 +1,9 @@
 #[cfg(feature = "memory-storage")]
 mod api_usage {
-    use gluesql::{memory_storage::MemoryStorage, prelude::Glue};
+    use gluesql::{
+        memory_storage::MemoryStorage,
+        prelude::{Glue, Payload, Value},
+    };
 
     pub async fn run() {
         let storage = MemoryStorage::default();
@@ -31,6 +34,37 @@ mod api_usage {
         )
         .await
         .unwrap();
+
+        let result = glue
+            .execute("SELECT * FROM api_test ORDER BY id ASC")
+            .await
+            .unwrap();
+
+        assert_eq!(
+            result,
+            vec![Payload::Select {
+                labels: (vec![
+                    String::from("id"),
+                    String::from("name"),
+                    String::from("nullable"),
+                    String::from("is")
+                ]),
+                rows: (vec![
+                    vec![
+                        Value::I64(1),
+                        Value::Str(String::from("test1")),
+                        Value::Str(String::from("not null")),
+                        Value::Bool(true),
+                    ],
+                    vec![
+                        Value::I64(2),
+                        Value::Str(String::from("test2")),
+                        Value::Null,
+                        Value::Bool(false),
+                    ],
+                ])
+            }]
+        );
     }
 }
 
